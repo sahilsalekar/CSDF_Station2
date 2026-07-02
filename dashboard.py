2696a47d98fb3fece0f846a81f192fe4adf974ed
sahilsalekar <sahilsalekar2000@gmail.com>
2026-07-02 12:09:40 +0100

    api link updated

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,7 @@ import requests
 from datetime import datetime
 
 class Dashboard:
-    def __init__(self, base_url='http://130.159.92.210/api'):
+    def __init__(self, base_url='https://csdf.cmac.strath.ac.uk/api'):
         self.base_url = base_url
 
     def get_free_reactor(self):
