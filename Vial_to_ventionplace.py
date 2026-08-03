35f801c16b193f8490522b17f1801c81357051e2
sahilsalekar <sahilsalekar2000@gmail.com>
2026-08-03 17:37:46 +0100

    optimized

--- Vial_to_ventionplace.py
+++ Vial_to_ventionplace.py
@@ -10,25 +10,25 @@ def Vial_to_ventionplace(client):
     try:
 
         # Robot to vention Pos
-        client.SendCommand("moveoneaxis 6 -743.301 1")
+        client.SendCommand("moveoneaxis 6 -743.301 3")
         reply = client.SendCommand("waitforeom")
         if reply == "0":
             print("Robot moved to vention_pos.")
-            client.SendCommand("movej 1 887.921 -57.195 305.335 21.405 109.165 -743.301") # (J5 109.165)
+            client.SendCommand("movej 1 916.34 -56.527 304.236 21.408 109.165 -743.289") # (J5 109.165)
             reply = client.SendCommand("waitforeom")
         
-            client.SendCommand("movec 1 -688.556 -682.052 761.624 -90.455 90 -180 2")
+            client.SendCommand("movec 1 -688.82 -679.296 770.656 -90.883 90 -180 2")
             reply = client.SendCommand("waitforeom")
         
 
             client.SendCommand("graspplate 117 60 10")
             reply = client.SendCommand("waitforeom")
 
-            client.SendCommand("movec 1 -688.571 -682.051 887.92 -90.454 90 -180 2")
+            client.SendCommand("movec 3 -688.809 -679.294 916.34 -90.883 90 -180 2")
             reply = client.SendCommand("waitforeom")
 
             # Home Pos
-            client.SendCommand("movej 1 1017.83 -2.902 180.537 178.063 103.542 -934.686")
+            client.SendCommand("movej 3 1017.83 -2.902 180.537 178.063 103.542 -934.686")
             reply = client.SendCommand("waitforeom")
 
             print("Sucessfully completed vention vial place")
