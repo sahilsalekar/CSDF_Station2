bd69000a7047b0a9aecb7b565dbb39b1866805eb
sahilsalekar <sahilsalekar2000@gmail.com>
2026-08-08 15:41:27 +0100

    server ip chnaged

--- robot_data.py
+++ robot_data.py
@@ -4,7 +4,7 @@ import time
 import datetime
 import requests 
 
-API_URL = "http://10.5.49.10:8000/robot-data"
+API_URL = "http://130.159.77.34:8000/robot-data"
 
 
 def _send_to_endpoint(payload: dict):
